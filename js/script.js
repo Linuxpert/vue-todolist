34907ccd7d51379072f4de966197d630f40d6ba9
Linuxpert <g.manfre10@gmail.com>
2021-11-12 18:25:29 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 var todolist = new Vue({
     el:"#app",
     data: {
+        newTask: [
+            {
+                text:"",
+                done:"false"
+            },
+        ],
         tasks: [
             {
                 text: 'andare a correre',
-                done: 'true'
+                done: 'false'
             },
             {
                 text: 'fare la spesa',
@@ -12,12 +18,31 @@ var todolist = new Vue({
             },
             {
                 text: 'fare il bucato',
-                done: 'true'
+                done: 'false'
             },
             {
                 text: 'sistemare la stanza',
-                done: 'true'
+                done: 'false'
             },
         ],
+    },
+    methods: {
+        removeItem(index){
+            this.tasks.splice(index,1);
+        },
+        aggiungiTask(){
+            this.tasks.push(this.newTask);
+            this.newTask = {
+                text:"",
+                done:"false"
+            };
+        },
+        doneItem(index){
+            if(this.tasks[index].done === false){
+                this.tasks[index].done = true;
+            }else{
+                this.tasks[index].done = false;
+            }
+        }
     }
 });
